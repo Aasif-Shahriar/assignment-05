9f0addcaa66fe646fffc46513726c7cb4488e107
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-03-01 21:07:17 +0600

    added blog questions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,42 +4,42 @@ for(let i = 0; i < buttons.length; i++){
     const button = buttons[i];
 
     button.addEventListener('click', function(){
+        //alert on click
         alert('Board updated Successfully');
 
+        // task assigned count increase and decrease
         const taskUpdate = document.getElementById('task-update');
         let convTaskUpdate = Number(taskUpdate.innerText);
         const taskCount = document.getElementById('task-count');
         let convTaskCount = Number(taskCount.innerText);
 
+        if(convTaskCount > 0){
+            //decrease
+            convTaskCount--;
+            taskCount.innerText = convTaskCount;
+            //increase
+            convTaskUpdate++;
+            taskUpdate.innerText = convTaskUpdate;
+            //disabled complete button
+            button.setAttribute('disabled', true);
+            button.style.background = '#9BA8F8';
+            //alert on completed all task
+            if(convTaskCount === 0){
+                alert("Congratulations! You've Completed All The Task. ");
+            }
+        }; 
 
+        //adding history card
         const clickedHistory = document.getElementById('clicked-history');
         const div = document.createElement('div');
         clickedHistory.appendChild(div);
         div.classList.add('bg-primary','p-4', 'rounded-lg');
         div.innerText = `You have Complete The Task Add Dark Mode at 12:48:15 PM`;
-
+        //clearing history card
         document.getElementById('clear-history-btn')
         .addEventListener('click', function(){
             clickedHistory.innerHTML = "";
-        })
-
-        if(convTaskCount > 0){
-            convTaskCount--;
-            taskCount.innerText = convTaskCount;
-
-            convTaskUpdate++;
-            taskUpdate.innerText = convTaskUpdate;
-
-            button.setAttribute('disabled', true);
-            button.style.background = '#9BA8F8';
-
-
-
-            if(convTaskCount === 0){
-                alert("Congratulations! You've Completed All The Task. ");
-            }
-        }; 
-        
+        });
     });
 
 }
---
+++ JavaScript/toggle-bg.js
@@ -0,0 +1,10 @@
+const bgChangeBtn = document.getElementById('toggle-bg-btn');
+const body = document.body;
+let colors = ['#ade8f4', '#b7b7a4', '#adb5bd', '#95d5b2', '#b5e48c','#edc4b3', '#c77dff', '#fff0f3', '#d7e3fc', '#99e2b4'];
+
+
+bgChangeBtn.addEventListener('click', function(){
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    body.style.backgroundColor = randomColor;
+    body.style.transition = '.5s'
+})
