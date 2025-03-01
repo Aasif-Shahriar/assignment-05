a4c4b8c0d0b7017627ac31b630b8eeb09e97cff4
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-03-01 17:28:33 +0600

    built logic on clear-histoy-btn

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,13 +4,24 @@ for(let i = 0; i < buttons.length; i++){
     const button = buttons[i];
 
     button.addEventListener('click', function(){
-        
         alert('Board updated Successfully');
 
         const taskUpdate = document.getElementById('task-update');
         let convTaskUpdate = Number(taskUpdate.innerText);
         const taskCount = document.getElementById('task-count');
         let convTaskCount = Number(taskCount.innerText);
+
+
+        const clickedHistory = document.getElementById('clicked-history');
+        const div = document.createElement('div');
+        clickedHistory.appendChild(div);
+        div.classList.add('bg-primary','p-4', 'rounded-lg');
+        div.innerText = `You have Complete The Task Add Dark Mode at 12:48:15 PM`;
+
+        document.getElementById('clear-history-btn')
+        .addEventListener('click', function(){
+            clickedHistory.innerHTML = "";
+        })
 
         if(convTaskCount > 0){
             convTaskCount--;
@@ -20,13 +31,14 @@ for(let i = 0; i < buttons.length; i++){
             taskUpdate.innerText = convTaskUpdate;
 
             button.setAttribute('disabled', true);
-
             button.style.background = '#9BA8F8';
+
+
 
             if(convTaskCount === 0){
                 alert("Congratulations! You've Completed All The Task. ");
             }
-        }
+        }; 
         
     });
 
